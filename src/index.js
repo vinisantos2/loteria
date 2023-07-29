2619881cd78243902e2c9237f37bc7d3d92b579a
vinisantos2 <m.vinicius.s.f.c@gmail.com>
2023-07-29 14:43:56 -0300

    finalizando layout

--- src/index.js
+++ src/index.js
@@ -12,6 +12,7 @@ root.render(
         <Cartela />
         <Resultados />
     </div>
+    
 );
 
 // If you want to start measuring performance in your app, pass a function
--- src/main/Resultados.jsx
+++ src/main/Resultados.jsx
@@ -3,12 +3,23 @@ import React, { Component } from 'react'
 
 export default class Resultados extends Component {
 
-    render() {
+    state = {
+        pontos15: 0,
+        pontos14: 3
+    }
 
+    mudar(e) {
+      
+        this.setState({pontos15: 8})
+        console.log(this.state.pontos15+"AQui");
+
+    }
+    render() {
+        const {pontos15 , pontos14} = this.state;
         return (
             <div>
                 <p>
-                    Total de jogos com 15 pontos:
+                    Total de jogos com 15 pontos: {pontos15}
                 </p>
                 <p>
                     Total de jogos com 14 pontos:
@@ -22,9 +33,10 @@ export default class Resultados extends Component {
                 <p>
                     Possivel retorno:
                 </p>
+                <button  onClick={e => this.mudar(e)}>ok</button>
 
             </div>
-            
+
         )
     }
 }
